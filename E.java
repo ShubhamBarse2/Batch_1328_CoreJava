2a74e633ec22410d319d7adc911b45102ef35f62
ShubhamBarse2 <shubhambarse510@gmail.com>
2026-05-27 16:04:37 +0530

    Add files via upload

---
+++ E.java
@@ -0,0 +1,47 @@
+
+public class E {
+
+	public static void main(String[] args) {
+
+//		for (int i = 1; i <= 10; i++) {
+//
+//			if (i == 6) {
+//				break;
+//			}
+//			System.out.println(i);
+//		}
+
+//		for (int i = 1; i <= 10; i++) {
+//
+//			if (i == 1) {
+//				continue;
+//			}
+//			System.out.println(i);
+//		}
+
+//		for (int i = 1; i <= 10; i++) {
+//
+//			if (i % 2 == 1) {
+//				continue;
+//			}
+//			System.out.println(i);
+//		}
+
+//		for (int i = 1; i <= 10; i++) {
+//
+//			if (i % 2 == 0) {
+//				break;
+//			}
+//			System.out.println(i);
+//		}
+
+//		for (char ch = 'a'; ch <= 'z'; ch++) {
+//			if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+//				continue;
+//			}
+//			System.out.println(ch);
+//		}
+
+	}
+
+}
