c67890317e722a77a7922257972caaa1294abb01
ShubhamBarse2 <shubhambarse510@gmail.com>
2026-05-28 16:06:04 +0530

    Add files via upload

---
+++ PackageRelated/src/com/demo/B.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import com.jbk.A;
+
+public class B {
+
+	public static void main(String[] args) {
+		com.jbk.A a = new A();
+		com.demo.A aa = new com.demo.A();
+		
+//		Scanner
+//		DriverManager
+		
+		
+	}
+}
