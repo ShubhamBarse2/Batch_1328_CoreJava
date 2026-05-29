b69fb27299d41d14ca65f614c9e4c2610fb0e39f
ShubhamBarse2 <shubhambarse510@gmail.com>
2026-05-29 16:26:10 +0530

    Add files via upload

---
+++ Pattern_ProgramCode/PractiseProgram.java
@@ -0,0 +1,64 @@
+package Pattern_ProgramCode;
+
+public class PractiseProgram {
+
+	public static void main(String[] args) {
+
+//		for (int i = 1; i <= 5; i++) { // Column
+//			for (int j = 1; j <= i; j++) { // Rows
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//
+//		}
+
+//		for (int i = 5; i >= 1; i--) { // Column
+//			for (int j = 1; j <= i; j++) { // Rows
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//
+//		}
+
+//		for (int i = 5; i >= 1; i--) {
+//
+//			for (int j = i; j <= 5; j++) {
+//				System.out.print(" ");
+//			}
+//
+//			for (int k = 1; k <= i; k++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//
+//		}
+
+//		for (int i = 1; i <= 5; i++) {
+//
+//			for (int j = 5; j >= i; j--) {
+//				System.out.print(" ");
+//			}
+//
+//			for (int k = 1; k <= i; k++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//
+//		}
+
+//		for (int i = 1; i <= 5; i++) {
+//
+//			for (int j = 1; j <= 5 - i; j++) {
+//				System.out.print(" ");
+//			}
+//
+//			for (int k = 1; k <= i; k++) {
+//				System.out.print("* ");
+//			}
+//			System.out.println();
+//
+//		}
+
+	}
+
+}
